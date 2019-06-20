3398a9ff4da035355cb0c1373805dbec82630664
propredator23 <christiandash@hotmail.com>
2019-06-20 16:46:11 -0700

    game
    Barebones of the game

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public Transform targetTransform;
+    public float speed;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        chase();
+	}
+    void chase()
+    {
+        Vector3 transformDistance = targetTransform.position - transform.position;
+        Vector3 directionToTarget = transformDistance.normalized;
+        Vector3 velocity = directionToTarget * speed;
+        float distanceToTarget = transformDistance.magnitude;
+
+        if (distanceToTarget > 2f)
+        {
+            rb.velocity = velocity;
+        }
+    }
+}
