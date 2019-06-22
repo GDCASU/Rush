59c94f2798fc580b6ec65cbc8f01e2d869ee913d
propredator23 <christiandash@hotmail.com>
2019-06-22 16:08:27 -0700

    game
    barebones

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject camera;
+	// Use this for initialization
+	void Start ()
+    {
+        camera.transform.rotation.Set(45, 0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        moveCamera();
+    }
+    void moveCamera()
+    {
+        camera.transform.localPosition = new Vector3(0,10, - 2.5f);
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        movement();
+	}
+    void movement()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        rb.velocity = velocity;
+    }
+}
