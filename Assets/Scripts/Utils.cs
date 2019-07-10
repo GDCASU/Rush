1d61fa88c73ce89c5924ecf7374bb590b43d02aa
content-deleted <contentdeleted@gmail.com>
2019-07-10 04:17:46 -0700

    wip: testing shadow casting on sprites

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public static class Utils
 {
+    const float bulletLiveSize=1.5f;
 	public static bool IsOnScreen(GameObject g) {
-        var vP = Camera.main.WorldToViewportPoint(g.transform.position);
+        var vP = Camera.main.WorldToViewportPoint(g.transform.position)/bulletLiveSize;
         return vP.x > 0 && vP.x < 1 && vP.y > 0 && vP.y < 1;
     }
 }
