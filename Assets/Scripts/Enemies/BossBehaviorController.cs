41ad7bf78cf662c47e5877bec97dfec8e1cdbde1
content-deleted <contentdeleted@gmail.com>
2019-07-27 13:07:55 -0700

    wip: boss controller to switch between behaviours

---
+++ Assets/Scripts/Enemies/BossBehaviorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(EnemyHealth))]
+public class BossBehaviorController : MonoBehaviour {
+    [Serializable]
+    public struct subBehaviour {
+        public MonoBehaviour controlledBehaviour;
+        public float activeHealthRangeMin;
+        public float activeHealthRangeMax;
+        public bool disableAfterTime;
+        public int activeFramesBeforeDisable;
+    }
+    public List<subBehaviour> controlledBehaviours;
+    private List<subBehaviour> activeBehaviours;
+    private List<subBehaviour> inactiveBehaviours;
+    public void Awake() {
+        foreach(subBehaviour b in controlledBehaviours){
+            b.controlledBehaviour.enabled = false;
+        }
+    }
+	void Update () {
+		
+	}
+}
