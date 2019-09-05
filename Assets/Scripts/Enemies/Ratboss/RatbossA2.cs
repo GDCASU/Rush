080aed66d585517fbdec8164495544c862ade82e
propredator23 <christiandash@hotmail.com>
2019-09-05 08:56:33 -0700

    Got most of A2 done (Tail Stab)

--- Assets/Scripts/Enemies/Ratboss/RatbossA2.cs
+++ Assets/Scripts/Enemies/Ratboss/RatbossA2.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 
 public class RatbossA2 : MonoBehaviour
 {
+    public GameObject tailPrefab;
+    public Sprite chargedTail;
+    public Sprite extendedTail;
+    public Sprite currentSprite;
+    public int lookingFrames;
+    public int curlFrames;
+    public int coolDownFrames;
+    private SpriteRenderer srTail;
+    private SpriteRenderer srBoss;
     private Transform player;
     private GameObject location;
+    private int attacksPerformed=0;
+
+
 
 
     private void Start()
@@ -20,16 +32,47 @@ public class RatbossA2 : MonoBehaviour
 
     IEnumerator TailStab()
     {
-        Vector3 midVector = Vector3.Lerp(player.transform.position, location.transform.position, 0.5f);
-        float distance = Vector2.Distance(player.transform.position, location.transform.position);
-        Vector2 origin = new Vector2(midVector.x, midVector.y);
+        while (attacksPerformed < 3)
+        {
+            srBoss = location.GetComponentInChildren<SpriteRenderer>();
+            srBoss.flipX = true;
+            GameObject tail = Instantiate(tailPrefab, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+            srTail = tail.GetComponent<SpriteRenderer>();
+            srTail.sprite = extendedTail;
+            tail.transform.SetParent(location.transform);
+            tail.transform.position = location.transform.position + new Vector3(2f, 0, 0);
+            //tail.transform.rotation= location.transform.rotation;
 
-        var hits = Physics2D.BoxCastAll(origin, new Vector2(distance,3), 0 ,player.transform.position);
 
-        var players = hits?.Where(x => x.transform.tag == "Player")?.Select(e => e.transform.GetComponent<PlayerHealth>());
-        foreach (PlayerHealth enemy in players) print("Sup");
+            for (int i = 0; i < lookingFrames; i++)
+            {
+                tail.transform.up = new Vector3(-player.position.x, player.position.y);
+                yield return new WaitForEndOfFrame();
+            }
+            srTail.sprite = chargedTail;
+            for (int i = 0; i < curlFrames; i++)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            srTail.sprite = extendedTail;
 
-        yield return new WaitForEndOfFrame();
+
+            Vector3 midVector = Vector3.Lerp(player.transform.position, location.transform.position, 0.5f);
+            float distance = Vector2.Distance(player.transform.position, location.transform.position);
+            Vector2 origin = new Vector2(midVector.x, midVector.y);
+
+            var hits = Physics2D.BoxCastAll(origin, new Vector2(distance, 3), 0, player.transform.position);
+
+            var players = hits?.Where(x => x.transform.tag == "Player")?.Select(e => e.transform.GetComponent<PlayerHealth>());
+            foreach (PlayerHealth enemy in players) print("You got Hit");
+
+            for (int i = 0; i < coolDownFrames; i++)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            attacksPerformed++;
+        }
     }
 
 }
