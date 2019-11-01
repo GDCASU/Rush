3debcf6090b68abd2bb3274d0e38f32387029689
narnier1 <narnier1@asu.edu>
2019-11-01 15:50:35 -0700

    Fix class names

--- Assets/Scripts/Enemies/Chimera/Chimera.cs
+++ Assets/Scripts/Enemies/Chimera/Chimera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chimera.cs : MonoBehaviour
+public class Chimera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Enemies/Chimera/Head.cs
+++ Assets/Scripts/Enemies/Chimera/Head.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Head.cs : MonoBehaviour
+public class Head : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
