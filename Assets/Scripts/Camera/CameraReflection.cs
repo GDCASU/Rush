3cb8f8ec243e08bc983012f638716055b4c2f9c2
NicholasNguyen99 <nicholas.ngoc.nguyen@gmail.com>
2019-11-09 19:50:46 -0700

    Script and Material
    Adds in a script to control the camera doing the reflections and messed with the water material

---
+++ Assets/Scripts/Camera/CameraReflection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class meant for the lady of the lake boss that
+/// controls the camera creating the player and other objects reflections
+/// </summary>
+public class CameraReflection : MonoBehaviour
+{
+    public float yOffSet = 2f;
+    public Transform playerCamera;
+
+    private void FixedUpdate()
+    {
+        Debug.Log(string.Format("This y = {0}, Player y = {1}", this.transform.position.y, playerCamera.position.y));
+
+        Vector3 thisPosition = this.transform.position;
+        Vector3 playerCamPosition = playerCamera.localPosition;
+
+        thisPosition.y = playerCamPosition.y + yOffSet;
+
+        this.transform.position = thisPosition;
+    }
+}
