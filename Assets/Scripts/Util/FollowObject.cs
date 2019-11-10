afba2ffffadf21800eaac1a58290e4d1788dda9d
NicholasNguyen99 <nicholas.ngoc.nguyen@gmail.com>
2019-11-09 20:39:07 -0700

    Player Reflection
    New reflection object added as prefab. Player in my scene currently has one on him as an example

---
+++ Assets/Scripts/Util/FollowObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class that follows a given objects transform in
+/// terms of x and y ONLY
+/// </summary>
+public class FollowObject : MonoBehaviour
+{
+    public float xOffSet;
+    public float yOffSet = -1f;
+    public Transform objTransform;
+
+    private void Update()
+    {
+        Vector3 newPosition = this.transform.position;
+        Vector3 objPosition = objTransform.position;
+
+        newPosition.x = objPosition.x + xOffSet;
+        newPosition.y = objPosition.y + yOffSet;
+
+        this.transform.position = newPosition;
+    }
+}
