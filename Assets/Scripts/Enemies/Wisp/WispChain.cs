b497320a41e86eb6052b525f9de67f30e65888c7
mschmi11 <54912416+mschmi11@users.noreply.github.com>
2019-11-22 14:52:01 -0700

    Boss Arena
    Boss sprites created. Folder structure for scripts created.

---
+++ Assets/Scripts/Enemies/Wisp/WispChain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispChain : MonoBehaviour
+{
+    public GameObject WispBoss, ChainCarrier;
+    private void Start()
+    {
+        GameObject c1 = Instantiate(ChainCarrier);
+
+    }
+}
