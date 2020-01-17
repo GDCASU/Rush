97523527c56a78cb3d9c31628b7f8a612a9674ca
propredator23 <christiandash@hotmail.com>
2020-01-17 02:54:18 -0700

    Updated Player Shot
    Updated the player shooting mechanic to more closely follow the documented mechanic wanted, look at documentation for full details.

--- Assets/Scripts/Player/PlayerBasicShot.cs
+++ Assets/Scripts/Player/PlayerBasicShot.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBasicShot : MonoBehaviour {
+
+    public bool charging = false;
+    public float chargeUpFrames;
 	public float bulletSpeed = 1;
     public Sprite bulletSprite;
     public float scale = 0.25f;
     public float shootFrameReset;
     private IInputPlayer player;
+    private float charge=0;
     
     private PlayerMovement pMovement;
 
@@ -17,19 +21,40 @@ public class PlayerBasicShot : MonoBehaviour {
         pMovement = GetComponent<PlayerMovement>();
     }
     private float timer = 0;
-    public void Update () {
-        if (InputManager.GetButton(PlayerInput.PlayerButton.Shoot, player) && timer < 0)
+    public void Update () 
+    {
+        if (InputManager.GetButtonUp(PlayerInput.PlayerButton.Shoot, player) && charging)
         {
-            pMovement.faceMouse();
-            Vector2 direction = pMovement.overRideFacing;
+            if (charge > chargeUpFrames)
+            {
+                pMovement.faceMouse();
+                Vector2 direction = pMovement.overRideFacing;
 
-            var sp = BulletPool.rent();
-            sp.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            sp.GetComponent<Bullet>().Init(direction, 0, bulletSpeed, Bullet.MoveFunctions.Spin, Bullet.SpawnFunctions.None, bulletSprite, Color.white, false, 0.15f);
+                var sp = BulletPool.rent();
+                sp.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                sp.GetComponent<Bullet>().Init(direction, 0, bulletSpeed, Bullet.MoveFunctions.Spin, Bullet.SpawnFunctions.None, bulletSprite, Color.white, false, 0.15f);
 
-            sp.transform.localScale = Vector3.one * scale;
-            timer = shootFrameReset;
+                sp.transform.localScale = Vector3.one * scale;
+            }
+            resetShot();
         }
+        if (GetComponent<PlayerHealth>().inv) resetShot();
+        if (InputManager.GetButtonDown(PlayerInput.PlayerButton.Shoot, player) && !charging && timer < 0)
+        {
+            charging = true;
+            charge = 0;
+        } 
+        if (InputManager.GetButton(PlayerInput.PlayerButton.Shoot, player) && charging )
+        {
+            charge++;
+        }
+        
         timer--;
+    }
+    void resetShot()
+    {
+        charging = false;
+        timer = (charge < chargeUpFrames) ? charge : shootFrameReset;
+        charge = 0;
     }
 }
