21e8ffff721572cb62fec14ca37a39068f749253
propredator23 <christiandash@hotmail.com>
2020-01-18 03:03:45 -0700

    Comments
    Added comments to the dwarf A1 and A2 as well as removed some uncessary logic/code

--- Assets/Scripts/Enemies/MoltenDwarf/MoltendwarfA1.cs
+++ Assets/Scripts/Enemies/MoltenDwarf/MoltendwarfA1.cs
@@ -5,7 +5,6 @@ using System;
 
 public class MoltendwarfA1 : BossAction
 {
-    const float tau = 2 * Mathf.PI;
     public Sprite bulletSprite;
     public Color bulletTint = Color.white;
     public Vector3 scale = new Vector3(0.05F, 0.05F, 0.05F);
@@ -16,72 +15,78 @@ public class MoltendwarfA1 : BossAction
     public float ArcOffset;
     public float bulletSpeed;
     public float returnSpeed;
-    public float bulletArc = tau;
     public float colliderRadius=0.5f;
+    /* ^ Will be replaced by
+    public float colliderSizeX=5f;
+    public float colliderSizeY =4f;
+    public float offsetX = 0.25f;
+    public float offsetY = 0.9f;
+    */
+
     private float currentOffset;
     private bool isBack;
 
 
-    public void OnEnable() => StartCoroutine(HammerThrow());
+    public void OnEnable() => StartCoroutine(HammerThrow());                        //Starts the hammer throwing action 
     IEnumerator HammerThrow()
     {
-        actionRunning = true;
+        actionRunning = true;                                                       //This line is  necessary to let the BossBehaviorController know the action is running.
         for (int x = 0; x < windUpAnimation; x++)
         {
-            if (x < 40) transform.Rotate(new Vector3(0, 0, 1.125f), Space.Self);
-            else transform.Rotate(new Vector3(0, 0, -2.25f), Space.Self);
+            if (x < 40) transform.Rotate(new Vector3(0, 0, 1.125f), Space.Self);    //This makes the sprite rotate 45 degreees clokwise in 40 frames
+            else transform.Rotate(new Vector3(0, 0, -2.25f), Space.Self);           //This makes the sprite rotate 45 degreees counter clockwse in 20 frames
             yield return new WaitForEndOfFrame();
         }
-        col.enabled = true;
-        spawnBullet();
+        col.enabled = true;                                                         //Makes the molten dwarf be able to take damage after it's done charging up and technically
+        spawnHammer();                                                              //After the hammer has been thrown
         for (int x = 0; x < hammerIsOut; x++)
         {
             yield return new WaitForEndOfFrame();
         }
         StartCoroutine(HammerBack());
     }
-    public void spawnBullet()
+    public void spawnHammer()                                                                               //A lot of this math is just to get the necessary arguments for the bullet.init
     {
-        Vector2 VectorToPlayer = PlayerHealth.singleton.transform.position - transform.position;
-        ArcOffset = (float)(Vector2.SignedAngle(Vector2.right, VectorToPlayer) * Math.PI / 180.0);
-        float angle = ArcOffset + currentOffset;
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        hammer = BulletPool.rent();
-        hammer.name = "Hammer";
-        hammer.GetComponent<Bullet>().onScreenDestroy = false;
-        hammer.transform.position = transform.position;
-        hammer.GetComponent<Bullet>().Init(direction, angle * (float)180.0 / Mathf.PI , bulletSpeed, Bullet.MoveFunctions.None, Bullet.SpawnFunctions.None, bulletSprite, bulletTint, true, colliderRadius, new List<float>());
-        hammer.transform.localScale = scale;
-    }
-    public void Update()
-    {
-        currentOffset %= bulletArc;
+        Vector2 VectorToPlayer = PlayerHealth.singleton.transform.position - transform.position;            //Vector math to get the direction of the player
+        ArcOffset = (float)(Vector2.SignedAngle(Vector2.right, VectorToPlayer) * Math.PI / 180.0);          //Math to have the rotation of the hammer
+        float angle = ArcOffset;                                                                            // |
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));                                // | 
+        hammer = BulletPool.rent();                                                                         //Creates a bullet from the rent method of the bulletpool script
+        hammer.name = "Hammer";                                                                             //Renames bullet to hammer, necessary for the hammerBack coroutine
+        hammer.GetComponent<Bullet>().onScreenDestroy = false;                                              //Makes it so that the hammer can go out of the screen and not be destroyed, necessary for the hammer to be able to hit the player on the wasy back       
+        hammer.transform.position = transform.position;                                                     //Makes the starting position of the hammer the center of the dwarf.
+        hammer.GetComponent<Bullet>().Init(direction, angle * (float)180.0 / Mathf.PI, bulletSpeed, Bullet.MoveFunctions.None, Bullet.SpawnFunctions.None, bulletSprite, bulletTint, true, colliderRadius, new List<float>());
+        /* ^ Will be replaced byt this once the bullet class gets updated
+        Destroy(hammer.GetComponent<CircleCollider2D>());                                                   //Removed the defualt circle collider 
+        hammer.AddComponent(typeof(BoxCollider2D));                                                         //Adds a box collider instead
+        hammer.GetComponent<BoxCollider2D>().isTrigger = true;                                              //Makes sure the box collider isTrigger
+        hammer.GetComponent<Bullet>().Init(direction, angle * (float)180.0 / Mathf.PI , bulletSpeed, Bullet.MoveFunctions.None, Bullet.SpawnFunctions.None, bulletSprite, bulletTint, true, colliderSizeX, colliderSizeY, null); //Initializes the hammer with all the calcualted properties
+        hammer.GetComponent<BoxCollider2D>().offset = new Vector2(offsetX, offsetY);                        //Makes sure that the collider is as centered as possible
+        */
+        hammer.transform.localScale = scale;                                                                //Makes sure that the hammer scale is bigger than the default otherwise it be to small to see
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Hammer") isBack = true;
+        if (collision.gameObject.name == "Hammer") isBack = true;                                            //Collision to detect when the hammer is back, necessary for the hammerBack coroutine to stop running          
     }
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Hammer") isBack = false;
+        if (collision.gameObject.name == "Hammer") isBack = false;                                           //Collision necessary to let the hammerBack coroutine that the hammer has left the moltenDwarf and start the loop  
     }
     IEnumerator HammerBack()
     {
-        if (hammer != null) hammer.GetComponent<Bullet>().enabled = false;
-        Vector2 VectorToDwarf = transform.position - hammer.transform.position;
-        ArcOffset = (float)(Vector2.SignedAngle(Vector2.right, VectorToDwarf) * Math.PI / 180.0);
-        float angle = ArcOffset + currentOffset;
-
-        Vector2 MoveVector = VectorToDwarf.normalized * returnSpeed;
-        while (!isBack && GameObject.Find("Hammer"))
-        {
+        if (hammer != null) hammer.GetComponent<Bullet>().enabled = false;                                   //Stops the hammer
+        Vector2 VectorToDwarf = transform.position - hammer.transform.position;                              //Vector math for the direction of the hammer towards the moltenDwarf
+        Vector2 MoveVector = VectorToDwarf.normalized * returnSpeed;                                         // |   
+        while (!isBack && GameObject.Find("Hammer"))                                                         //The reason why we use the isBack variable is because otherwise the hammer would destory itself when its spawned
+        {                                                                                                    //Loop that moves the hammer towards the direction 
             Vector2 m = MoveVector * Time.deltaTime;
             hammer.transform.localPosition = hammer.transform.localPosition + new Vector3(m.x, m.y, 0);
 
             yield return new WaitForEndOfFrame();
         }
-        col.enabled = false;
         Destroy(hammer);
-        actionRunning = false;
+        col.enabled = false;                                                                                 //Disables the dwarfs hitbox
+        actionRunning = false;                                                                               //Lets the BossBehaviorController know that the action is done running
     }
 }
--- Assets/Scripts/Enemies/MoltenDwarf/MoltendwarfA2.cs
+++ Assets/Scripts/Enemies/MoltenDwarf/MoltendwarfA2.cs
@@ -15,32 +15,31 @@ public class MoltendwarfA2 : BossAction
         actionRunning = true;
         for (int x = 0; x < yeetFrames; x++)
         {
-            if (x < 40) transform.Rotate(new Vector3(0, 0, -1.125f), Space.Self);
-            else transform.Rotate(new Vector3(0, 0, 4.5f), Space.Self);
-            if (x >= 40 && x % 6 == 0)
-            {
+            if (x < 40) transform.Rotate(new Vector3(0, 0, -1.125f), Space.Self);           //This makes the sprite rotate 45 degreees counter clokwise in 40 frames
+            else transform.Rotate(new Vector3(0, 0, 4.5f), Space.Self);                     //This makes the sprite rotate 90 degreees clokwise in 20 frames                        
+            if (x >= 40 && x % 6 == 0)                                                      
+            {                                                                               //This lets the bulletSpawner spawn a bullet every 1/3 of those 20 frames or avery 6.66 frames
                 spawner.enabled = true;
                 spawner.enabled = false;
             }
             yield return new WaitForEndOfFrame();
 
+            //Alternate way for the bullets to spawn
             /*
-             if (x < 50) transform.Rotate(new Vector3(0, 0, -0.9f), Space.Self);
+             if (x < 50) transform.Rotate(new Vector3(0, 0, -0.9f), Space.Self);            
             else transform.Rotate(new Vector3(0, 0, 9), Space.Self);
             if(x==50) spawner.enabled = true;
             yield return new WaitForEndOfFrame();
             */
         }
-        actionRunning = false;
+        actionRunning = false;                                                                  //Lets the BossBehaviorController its done with the action
     }
-    IEnumerator TriShotEnd()
+    IEnumerator TriShotEnd()                                
     {
-        for (int x = 0; x < returnFrames; x++)
+        for (int x = 0; x < returnFrames; x++)  
         {
-            transform.Rotate(new Vector3(0, 0, -4.5f), Space.Self);         
+            transform.Rotate(new Vector3(0, 0, -4.5f), Space.Self);                             //This makes the sprite rotate 45 degreees counter clokwise in 10 frames
             yield return new WaitForEndOfFrame();
         }
-        spawner.enabled = false;
-        yield return new WaitForEndOfFrame();
     }
 }
