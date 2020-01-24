74508183e5cf668e98ca18cf7083c116f4f45ec6
Brad <phyrol39@gmail.com>
2020-01-24 16:50:35 -0700

    Dwarf Movement
    Added a movement script to the boss so he slowly moves towards the player and stops a certain distance away.
    
    Also changed the boss sprite from a random png to a png of a sketch.

---
+++ Assets/Scripts/Enemies/MoltenDwarf/MoltenDwarfMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoltenDwarfMove : MonoBehaviour
+{
+    public GameObject myPlayer;
+
+    private float speed;
+    private float step;
+
+    private Vector2 playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = myPlayer.transform.position;
+
+        if(Vector2.Distance(this.transform.position, playerPosition) > 5.0f)
+        {
+            MoveToPlayer(playerPosition);
+        }
+    }
+
+    void MoveToPlayer(Vector2 target)
+    {
+        step = speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, playerPosition, step);
+    }
+}
