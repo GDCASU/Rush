719ec08818e3c7d05d71eee846eae58510f9db8a
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-01-26 17:08:07 -0700

    Basic turret movement

---
+++ Assets/Scripts/Enemies/WispBoss/TurretControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour
+{
+    [Header("Turret Travel")]
+    public GameObject targetLocationObj;
+    public float movementSpeed;
+    private Vector3 NormalizedTargetVector
+    {
+        get
+        {
+            Vector3 rval = targetLocationObj.transform.position - transform.position;
+            rval = rval.normalized;
+
+            return rval;
+        }
+    }
+    private bool _isMoving = false;
+
+    private void Start()
+    {
+        _isMoving = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if(_isMoving)
+        {
+            transform.position += NormalizedTargetVector * movementSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Location"))
+            _isMoving = false;
+    }
+}
