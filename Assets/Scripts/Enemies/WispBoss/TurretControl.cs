d9178d2862accd794af60ad18de78be0972da4bc
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-01-26 18:04:53 -0700

    Turret arch movement

--- Assets/Scripts/Enemies/WispBoss/TurretControl.cs
+++ Assets/Scripts/Enemies/WispBoss/TurretControl.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class that will control the WispBoss movement and fire.
+/// 
+/// Created by Nicholas Nguyen
+/// </summary>
 public class TurretControl : MonoBehaviour
 {
     [Header("Turret Travel")]
     public GameObject targetLocationObj;
     public float movementSpeed;
-    private Vector3 NormalizedTargetVector
+    private Vector3 NormalizedTargetVector  //Normalized vector that points from this obj to the target obj
     {
         get
         {
@@ -17,7 +22,15 @@ public class TurretControl : MonoBehaviour
             return rval;
         }
     }
+    public Vector3 startingDirection;   //This is the initial direction for the arch feature
+    private Vector3 _currentDirection;
+    public float archModifier;  //Decrease to create less arch and Increase for the opposite. Neutral value is 1
     private bool _isMoving = false;
+
+    private void Awake()
+    {
+        _currentDirection = startingDirection;
+    }
 
     private void Start()
     {
@@ -28,12 +41,16 @@ public class TurretControl : MonoBehaviour
     {
         if(_isMoving)
         {
-            transform.position += NormalizedTargetVector * movementSpeed;
+            Vector3 newPos = NormalizedTargetVector + _currentDirection;
+            _currentDirection = newPos * archModifier;
+
+            transform.position += newPos.normalized * movementSpeed;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Once the turret has reached it's target location stop it from moving
         if (collision.gameObject.name.Contains("Location"))
             _isMoving = false;
     }
