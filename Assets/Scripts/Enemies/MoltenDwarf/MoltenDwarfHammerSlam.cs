f0eee5c15b9db3cd21a3925b8802f563a58c3ec2
Brad <phyrol39@gmail.com>
2020-02-07 14:58:26 -0700

    Minor Changes
    - Created script of hammer slam
    - added comments to movement script
    - Added sprite flip when player moves around boss

---
+++ Assets/Scripts/Enemies/MoltenDwarf/MoltenDwarfHammerSlam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoltenDwarfHammerSlam : MonoBehaviour
+{
+    private Animator dwarfAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dwarfAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Enemies/MoltenDwarf/MoltenDwarfMove.cs
+++ Assets/Scripts/Enemies/MoltenDwarf/MoltenDwarfMove.cs
@@ -12,35 +12,51 @@ public class MoltenDwarfMove : MonoBehaviour
     private Vector2 playerPosition;
 
     private Animator dwarfAnim;
+    private SpriteRenderer dwarfSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        dwarfAnim = GetComponent<Animator>();
+        dwarfAnim = GetComponent<Animator>();   //get the animator
+        dwarfSprite = GetComponent<SpriteRenderer>();   //get the sprite renderer
 
-        speed = 1.5f;
+        speed = 1.5f;  //movement speed set
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = myPlayer.transform.position;
+        playerPosition = myPlayer.transform.position;  //position of player to use
 
-        if(Vector2.Distance(this.transform.position, playerPosition) > 3.0f)
+        if(Vector2.Distance(this.transform.position, playerPosition) > 3.0f)  //distance between dwarf and player
         {
             MoveToPlayer(playerPosition);
-            dwarfAnim.SetBool("isAttacking", false);
+            dwarfAnim.SetBool("isAttacking", false); // activates attack animation
         }
         else
         {
             dwarfAnim.SetBool("isAttacking", true);
         }
+
+        CheckFacing();
     }
 
     void MoveToPlayer(Vector2 target)
     {
-        step = speed * Time.deltaTime;
+        step = speed * Time.deltaTime; //movement speed
 
-        transform.position = Vector2.MoveTowards(this.transform.position, playerPosition, step);
+        transform.position = Vector2.MoveTowards(this.transform.position, playerPosition, step); //moves dwarf to player
+    }
+
+    void CheckFacing()
+    {
+        if(playerPosition.x > gameObject.transform.position.x) //if player is on the RIGHT side of the dwarf
+        {
+            dwarfSprite.flipX = true;
+        }
+        else //if player is on left side of dwarf
+        {
+            dwarfSprite.flipX = false;
+        }
     }
 }
