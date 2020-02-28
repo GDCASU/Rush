c73d3ea192e039245f5faa8d9584af6875d9ffb7
Brad <phyrol39@gmail.com>
2020-02-28 16:02:38 -0700

    Initial setup and Initial TongueAttack
    - Setup of folders and placeholder frog sprite
    - Beginning of TongueAttack script
    - Made KingFrog scene

---
+++ Assets/Scripts/Enemies/KingFrog/KingFrogParent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingFrogParent : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject myPlayer;
+}
