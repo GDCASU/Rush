dbc3fe354200e755ecd30f0059599bc4ed25ebf9
propredator23 <christiandash@hotmail.com>
2020-04-02 21:28:38 -0700

    First Commit
    Basic script and scene setup

---
+++ Assets/Scripts/Enemies/RageMage/RageMageA5.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageMageA5 : MonoBehaviour
+{
+    public int chargePunchFrames;
+    void Start()
+    {
+        
+    }
+
+   IEnumerator punch()
+    {
+        for (int x = 0; x < chargePunchFrames; x++)
+        {
+            yield return new WaitForSeconds(.1666666666666f);
+        }
+
+    
+    }
+    
+}
