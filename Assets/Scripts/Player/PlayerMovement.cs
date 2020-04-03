21669328df22d84000bc5843ee1e2714c0604596
propredator23 <christiandash@hotmail.com>
2020-04-03 10:47:27 -0700

    Knockback
    Added the knockback mechanic alot of people wanted

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     private IInputPlayer player;
     private SpriteRenderer sp;
     private AnimationController anim;
+    private bool collided;
     public Vector2 velocity;
     public bool inControl=true; // player has direct control over movement
     public bool freezeInPlace; // player cannot move at all
@@ -35,10 +36,20 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     void Update ()
     {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            playerKnockback(-transform.right, 2f, .5f, 1f);
+        }
         if(!freezeInPlace) CheckMovementInput();
         checkFaceMouse();
         flipSprite();
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collided = true;
+    }
+
     public Vector2 overRideFacing = Vector2.zero;
     public Vector2 facing = Vector2.up;
     private Vector2 lastVel;
@@ -70,7 +81,32 @@ public class PlayerMovement : MonoBehaviour
     void flipSprite () {
        sp.flipX = (facing.x < 0);
     }
-
+    void playerKnockback(Vector3 direction, float knockBackSeconds, float stunSeconds, float speed)
+    {
+        StartCoroutine(knockback(direction,knockBackSeconds,stunSeconds,speed));
+    }
+    IEnumerator knockback(Vector3 direction, float knockBackSeconds,float stunSeconds, float speed)
+    {
+        float framerate=60;
+        WaitForSeconds ws = new WaitForSeconds(1 / framerate);
+        GetComponent<PlayerMovement>().freezeInPlace = true;
+        GetComponent<PlayerBasicMelee>().enabled = false;
+        GetComponent<PlayerBasicShot>().enabled = false;
+        GetComponent<PlayerDash>().enabled = false;
+        int x = 0;
+        while(x < knockBackSeconds * framerate && !collided)
+        {
+            rb.MovePosition(transform.position+(direction * speed));
+            x++;
+            yield return ws;
+        }
+        for (int y = 0; y < stunSeconds * framerate; y++) yield return ws;
+        GetComponent<PlayerMovement>().freezeInPlace = false;
+        GetComponent<PlayerBasicMelee>().enabled = true;
+        GetComponent<PlayerBasicShot>().enabled = true;
+        GetComponent<PlayerDash>().enabled = true;
+        collided = false;
+    }
     void LateUpdate () {
         // Update animations
         if(lastVel.magnitude > 0)
