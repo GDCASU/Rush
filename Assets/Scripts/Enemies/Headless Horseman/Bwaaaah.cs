b3389a14354354fadc53fdcd43ca250707f87098
danielwaker <dver99@sbcglobal.net>
2020-04-11 14:42:41 -0500

    Laser
    some laser stuff

--- Assets/Scripts/Enemies/Headless Horseman/Bwaaaah.cs
+++ Assets/Scripts/Enemies/Headless Horseman/Bwaaaah.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bwaaaah : MonoBehaviour
+public class Bwaaaah : BossAction
 {
+    public int aimFrames;
+    public int laserFrames;
 
     // Update is called once per frame
     void Update()
@@ -15,16 +17,21 @@ public class Bwaaaah : MonoBehaviour
 
     IEnumerator ImFiringMahLaser()
     {
-        //actionRunning = true;
+        actionRunning = true;
+        Transform player = PlayerHealth.singleton.transform;
+        //transform.GetChild(2).transform.LookAt(player, Vector3.right);
+        for (int i = 0; i < aimFrames; i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
         transform.GetChild(2).GetComponent<BoxCollider2D>().enabled = true;
-        transform.GetChild(2).GetComponent<MeshRenderer>().enabled = true;
-
-        for (int i = 0; i < 300; i++)
+        transform.GetChild(2).GetComponent<MeshRenderer>().enabled = true;      
+        for (int i = 0; i < laserFrames; i++)
         {
             yield return new WaitForEndOfFrame();
         }
         transform.GetChild(2).GetComponent<MeshRenderer>().enabled = false;
         transform.GetChild(2).GetComponent<BoxCollider2D>().enabled = false;
-        //actionRunning = false;
+        actionRunning = false;
     }
 }
